ada24f63265d7712bdbb84a5b2975e1f07f068dd
Owain <owainjones@acadamytrainee.com>
2019-09-11 09:49:55 +0100

    @Before and @After added to SeleniumTest

--- src/main/java/com/qa/GoogleSelenitumTest.java
+++ src/main/java/com/qa/GoogleSelenitumTest.java
@@ -1,10 +1,22 @@
 package com.qa;
 
+import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleSelenitumTest {
 
     private ChromeDriver driver;
-    System.setProperties()
+
+    @Before
+    public void setup(){
+        System.setProperty("web.chrome.driver","resources/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown(){
+        driver.close();
+    }
 
 }
