85796b12cfc1faa6d658814096a807e82ac4a5ac
Owain <owainjones@acadamytrainee.com>
2019-09-11 10:12:19 +0100

    Test searches for funny cat pictures

--- src/main/java/com/qa/GoogleSelenitumTest.java
+++ src/main/java/com/qa/GoogleSelenitumTest.java
@@ -1,8 +1,13 @@
 package com.qa;
 
-import org.junit.After;
-import org.junit.Before;
-import org.openqa.selenium.chrome.ChromeDriver;
+        import org.junit.After;
+        import org.junit.Before;
+        import org.junit.Test;
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.chrome.ChromeDriver;
+
+        import static org.junit.Assert.assertTrue;
 
 public class GoogleSelenitumTest {
 
@@ -10,13 +15,28 @@ public class GoogleSelenitumTest {
 
     @Before
     public void setup(){
-        System.setProperty("web.chrome.driver","resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\IdeaProjects\\seleniumtesting\\src\\main\\java\\resources\\chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
     @After
     public void tearDown(){
         driver.close();
+    }
+
+    @Test
+    public void searchTest() throws InterruptedException {
+        driver.get("http://google.com");
+        Thread.sleep(2000);
+        WebElement searchField = driver.findElement(By.name("q"));
+        assertTrue(searchField.isDisplayed());
+        searchField.sendKeys("funny cat pictures");
+        Thread.sleep(2000);
+        searchField.submit();
+        WebElement linkToBiggerPicture = driver.findElementByLinkText("Images for funny cat");
+        linkToBiggerPicture.click();
+        Thread.sleep(2000);
     }
 
 }
